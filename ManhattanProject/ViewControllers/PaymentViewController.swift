e0538cd821be86fa743514b28d1cf1e5cecfdc75
Nadia <joystate@me.com>
2018-04-22 18:23:47 -0400

    add web view in code

--- ManhattanProject/ViewControllers/PaymentViewController.swift
+++ ManhattanProject/ViewControllers/PaymentViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 import WebKit
 
 class PaymentViewController: UIViewController, WKNavigationDelegate {
-    
-    @IBOutlet weak var webView: WKWebView!
+
+    @IBOutlet weak var webViewContainer: UIView!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
+
+    var webView: WKWebView!
     
     private var url: URL?
     
@@ -25,14 +27,22 @@ class PaymentViewController: UIViewController, WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         guard let url = url else {
             return
         }
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: webViewContainer.frame, configuration: webConfiguration)
+        webViewContainer.addSubview(webView!)
         webView.alpha = 0
         webView.navigationDelegate = self
         webView.load(URLRequest(url: url))
     }
-    
+
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         UIView.animate(withDuration: 0.3, animations: {
             self.webView.alpha = 1
